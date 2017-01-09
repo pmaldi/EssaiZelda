092cfe021fd3133a496d66a754d25ca7471b1f7f
Patrice <Patrice@PC-PATRICE>
2017-01-09 02:48:44 +0100

    Add project files.

---
+++ EssaiZelda/Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace EssaiZelda
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Map MaMap = new Map();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = MaMap.MAP_WIDTH;  // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = MaMap.MAP_HEIGHT;   // set this value to the desired height of your window
+            graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            System.Diagnostics.Debug.WriteLine("Début de chargement des textures...");
+            MaMap.grassCenter = Content.Load<Texture2D>("images/grassCenter");
+            MaMap.liquidLava = Content.Load<Texture2D>("images/liquidLava");
+            MaMap.liquidWater = Content.Load<Texture2D>("images/liquidWater");
+            MaMap.snowCenter = Content.Load<Texture2D>("images/snowCenter");
+            MaMap.stoneCenter = Content.Load<Texture2D>("images/stoneCenter");
+            System.Diagnostics.Debug.WriteLine("Début de chargement des textures terminées...");
+            MaMap.Load();
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            MaMap.Draw(gameTime,spriteBatch); // On Appel la fonction Draw de la Class Map
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ EssaiZelda/Map.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssaiZelda
+{
+    class Map
+    {
+        public int MAP_HEIGHT = 700;
+        public int MAP_WIDTH = 700;
+        public int TILE_HEIGHT = 70;
+        public int TILE_WIDTH = 70;
+
+        public Texture2D grassCenter;
+        public Texture2D liquidLava;
+        public Texture2D liquidWater;
+        public Texture2D snowCenter;
+        public Texture2D stoneCenter;
+
+
+        int[,] Carte = {
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 2, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 2, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 3, 1, 1, 1 },
+            { 1, 1, 1, 2, 1, 1, 1, 1, 1, 1 },
+            { 1, 5, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 4, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        };
+
+        public Texture2D[] TileTextures = new Texture2D[6]; // On défini un Tableau de Texture2D de taille 6 élements [0-5]
+        
+        public void Load()
+        {
+            System.Diagnostics.Debug.WriteLine("Début d'affectation des Tiles...");
+            TileTextures[0] = null;
+            TileTextures[1] = grassCenter;
+            TileTextures[2] = liquidLava;
+            TileTextures[3] = liquidWater;
+            TileTextures[4] = snowCenter;
+            TileTextures[5] = stoneCenter;
+            System.Diagnostics.Debug.WriteLine("Début d'affectation des Tiles terminées...");
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            int c, l, id;
+            for (l=0; l< 10; l++) // Notre carte possede 10 Lignes
+            {
+                for (c = 0; c < 10; c++) // Et également 10 Colonnes
+                {
+                    id = Carte[l,c]; //recupere le numéro de la tile
+                    spriteBatch.Begin();
+                    int x = c * TILE_WIDTH; //Position X de la tile
+                    int y = l * TILE_HEIGHT; //Position Y de la tile
+                    spriteBatch.Draw(TileTextures[id], new Rectangle(x, y, TILE_HEIGHT, TILE_WIDTH), Color.White); // On déssine la tile a X et Y d'une longeur TILE_HEIGHT & TILE_WIDTH
+                    spriteBatch.End();
+                }
+            }
+        }
+    }
+}
